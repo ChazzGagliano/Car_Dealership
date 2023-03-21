a4caa639ef4cc2e9f7c930c694f3e3bd67652aec
Chazz Gagliano <cgags17285@gmail.com>
2023-03-21 16:06:04 -0400

    Car1, Car2, Car3 added

---
+++ store_item.rb
@@ -0,0 +1,10 @@
+
+#   # car1 = {"color" => "red", "price" => 90_000}
+#   # car2 = {"color" => "blue", "price" => 75_000}
+
+
+#   # p car1["color"]
+ 
+#  car3 = {color: "Green", model: "Honda", price: 60_000}
+
+#  p car3[:price]
