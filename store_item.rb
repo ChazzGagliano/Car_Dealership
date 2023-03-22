6195f2d4b5ea9f80065d26cd235508677dd5d7b2
Chazz Gagliano <cgags17285@gmail.com>
2023-03-22 14:35:25 -0400

    Store Class

--- store_item.rb
+++ store_item.rb
@@ -8,3 +8,34 @@
 #  car3 = {color: "Green", model: "Honda", price: 60_000}
 
 #  p car3[:price]
+
+class Store
+   attr_reader :make, :color, :year, :price
+   attr_writer :make
+
+  def initialize
+     @make = input_options[:make]
+     @color = input_options[:color]
+     @year = input_options[:year]
+     @price = input_options[:price]
+  end
+
+  def print_info
+  p "#{@make}, #{color} is from #{year}. It is #{price}.
+  "
+  end
+  
+  
+end
+
+car1 = Store.new(
+  {
+    :make => "Honda"
+    :color => "Blue"
+    :year => "2015"
+    :price => 40_000
+
+  }
+)
+
+car1.print_info
